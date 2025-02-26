bb8346b93d2ee4b109bf936c720f08ce4b4017d7
Ruslan <ruslan.konoplya@gmail.com>
2025-02-26 16:26:22 +0200

    gfdgfd

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
-import VitePluginGhPages from 'vite-plugin-gh-pages';
+
 
 export default defineConfig({
   base: '/ruslan/',  // Укажи имя репозитория на GitHub
-  plugins: [VitePluginGhPages()],
+  
 });
