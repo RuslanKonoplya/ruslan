46f6c10e6fa21f05a2ae5d89f1a5c4cc28772496
Ruslan <ruslan.konoplya@gmail.com>
2025-02-27 10:29:00 +0200

    bububu

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
 import './styles/style.scss'
 
+
+
+const button = document.querySelector('.red');
+const text = document.querySelector('.text')
+
+
+button.addEventListener('click', () => {
+  button.style = 'display : none';
+  text.textContent = 'БУБУБУ';
+  text.style.fontSize = '40px'
+
+})
+
